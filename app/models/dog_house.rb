35597c0df168f1b0945a33458cec59bd56afb757
mukandalee <leonardmukanda254@gmail.com>
2023-03-22 20:21:41 +0300

    complete

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,4 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  #dependent: :destroy   ----will tell Active Record to delete all the associated records when the parent record is deleted.
+  has_many :reviews, dependent: :destroy
 end
